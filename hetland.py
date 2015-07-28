fe66fd5859c6ad002fbc792170af80c208f5d9f0
miikka-cloudsage <donmiguel89@gmail.com>
2015-07-28 08:43:11 +0000

    alku

---
+++ hetland.py
@@ -0,0 +1,7 @@
+summa = input("Give a number: ")
+
+while summa < 100:
+    more = input("Give more: ")
+    summa = summa + more
+    
+print "Done"
---
+++ hetland2.py
@@ -0,0 +1,7 @@
+summa = input("give a number: ")
+
+for number in range (1,10):
+    number = input("give a number: ")
+    summa = summa + number
+    
+print summa
