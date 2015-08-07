c56efd3f20b86e02eebe3375379e75be65ee580c
miikka-cloudsage <donmiguel89@gmail.com>
2015-08-07 21:57:04 +0000

    ijloi

--- hetland.py
+++ hetland.py
@@ -1,7 +1,7 @@
-summa = input("Give a number: ")
+summa = input("Give a number (will stop when we hit over 100): ")
 
 while summa < 100:
     more = input("Give more: ")
     summa = summa + more
-    
+
 print "Done", "(The sum is", summa, ")"
--- hetland2.py
+++ hetland2.py
@@ -1,7 +1,7 @@
-summa = input("give a number: ")
+summa = input("give 1 number now, then keep giving more when prompted (10 in total): ")
 
 for number in range (1,10):
-    number = input("give a number: ")
+    number = input("give next number: ")
     summa = summa + number
-    
-print summa
+
+print "In total ", summa
